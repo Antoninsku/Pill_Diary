0a9e9a7728a12b70006c7e354e91530fd8ae0cc8
trananh2905 <64545331+trananh2905@users.noreply.github.com>
2022-02-16 01:38:37 +0200

    Create new Activity (Fragment) with BottomNavigationView

--- app/src/main/java/fi/antonina/pilldiary/MainActivity.java
+++ app/src/main/java/fi/antonina/pilldiary/MainActivity.java
@@ -2,25 +2,31 @@ package fi.antonina.pilldiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView createNewAccount;
+    Button goInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Create a new account Click Event with lambda
         createNewAccount = findViewById(R.id.new_Account_Create);
-        createNewAccount.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"Create a new account!",Toast.LENGTH_SHORT).show();
-            }
-        });
+        createNewAccount.setOnClickListener(view ->
+                Toast.makeText(getApplicationContext(),"Create a new account!",Toast.LENGTH_SHORT).show());
+
+        //GOIN Button Click event, move to MainFragment lambda
+        goInButton = findViewById(R.id.goInButton);
+        goInButton.setOnClickListener(view -> {
+            Toast.makeText(getApplicationContext(), "Login Successfully", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, MainFragment.class);
+            startActivity(intent);
+            });
+        }
     }
-}
