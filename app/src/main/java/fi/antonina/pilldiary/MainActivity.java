a24fa6595b1d725f43d67e68555ce542fccd4048
trananh2905 <64545331+trananh2905@users.noreply.github.com>
2022-02-15 23:09:12 +0200

    "Create new account!" clickable

--- app/src/main/java/fi/antonina/pilldiary/MainActivity.java
+++ app/src/main/java/fi/antonina/pilldiary/MainActivity.java
@@ -3,12 +3,24 @@ package fi.antonina.pilldiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView createNewAccount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createNewAccount = findViewById(R.id.new_Account_Create);
+        createNewAccount.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Create a new account!",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
